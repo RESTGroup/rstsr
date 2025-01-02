1fc8ce8fe3440e347362a5a9d3ce1b1e2ddb1d63
ajz34 <ajz34@outlook.com>
2025-01-02 12:43:15 +0800

    add arithmetic suffix for operator-overrideable device impls

--- rstsr-core/src/device_faer/operators/mod.rs
+++ rstsr-core/src/device_faer/operators/mod.rs
@@ -1,3 +1,3 @@
-pub mod op_binary;
-pub mod op_ternary;
+pub mod op_binary_arithmetic;
+pub mod op_ternary_arithmetic;
 pub mod op_with_func;
--- rstsr-core/src/storage/operators/mod.rs
+++ rstsr-core/src/storage/operators/mod.rs
@@ -1,7 +1,7 @@
-pub mod op_binary;
-pub mod op_ternary;
+pub mod op_binary_arithmetic;
+pub mod op_ternary_arithmetic;
 pub mod op_with_func;
 
-pub use op_binary::*;
-pub use op_ternary::*;
+pub use op_binary_arithmetic::*;
+pub use op_ternary_arithmetic::*;
 pub use op_with_func::*;
