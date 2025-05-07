4d9920ae3ece3e79dd92d653f1103d375502c427
ajz34 <ajz34@outlook.com>
2025-05-07 22:33:46 +0800

    openblas -- change linalg func testing to rstsr

--- rstsr-openblas/tests/test_linalg_func/func_f64.rs
+++ rstsr-openblas/tests/test_linalg_func/func_f64.rs
@@ -1,7 +1,5 @@
-use rstsr_core::prelude::*;
+use rstsr::prelude::*;
 use rstsr_core::prelude_dev::fingerprint;
-use rstsr_linalg_traits::traits_def::*;
-use rstsr_openblas::DeviceOpenBLAS as DeviceBLAS;
 use rstsr_test_manifest::get_vec;
 
 #[cfg(test)]
@@ -14,15 +12,15 @@ mod test {
         let mut b = rt::asarray((get_vec::<f64>('b'), [1024, 1024].c(), &device));
 
         // default
-        let c = cholesky(b.view());
+        let c = rt::linalg::cholesky(b.view());
         assert!((fingerprint(&c) - 43.21904478556176).abs() < 1e-8);
 
         // upper
-        let c = cholesky((b.view(), Upper));
+        let c = rt::linalg::cholesky((b.view(), Upper));
         assert!((fingerprint(&c) - -25.925655124816647).abs() < 1e-8);
 
         // mutable changes itself
-        cholesky((b.view_mut(), Upper));
+        rt::linalg::cholesky((b.view_mut(), Upper));
         assert!((fingerprint(&b) - -25.925655124816647).abs() < 1e-8);
     }
 
@@ -33,27 +31,27 @@ mod test {
         let b = rt::asarray((get_vec::<f64>('b'), [1024, 1024].c(), &device));
 
         // default, a
-        let (w, v) = eigh(a.view()).into();
+        let (w, v) = rt::linalg::eigh(a.view()).into();
         assert!((fingerprint(&w) - -71.4747209499407).abs() < 1e-8);
         assert!((fingerprint(&v.abs()) - -9.903934930318247).abs() < 1e-8);
 
         // upper, a
-        let (w, v) = eigh((a.view(), Upper)).into();
+        let (w, v) = rt::linalg::eigh((a.view(), Upper)).into();
         assert!((fingerprint(&w) - -71.4902453763506).abs() < 1e-8);
         assert!((fingerprint(&v.abs()) - 6.973792268793419).abs() < 1e-8);
 
         // default, a b
-        let (w, v) = eigh((a.view(), b.view())).into();
+        let (w, v) = rt::linalg::eigh((a.view(), b.view())).into();
         assert!((fingerprint(&w) - -89.60433120129908).abs() < 1e-8);
         assert!((fingerprint(&v.abs()) - -5.243112559130817).abs() < 1e-8);
 
         // upper, a b, itype=3
-        let (w, v) = eigh((a.view(), b.view(), Upper, 3)).into();
+        let (w, v) = rt::linalg::eigh((a.view(), b.view(), Upper, 3)).into();
         assert!((fingerprint(&w) - -2503.84161931662).abs() < 1e-8);
         assert!((fingerprint(&v.abs()) - 152.17700520642055).abs() < 1e-8);
 
         // mutable changes a
-        let (w, _) = eigh(a.view_mut()).into();
+        let (w, _) = rt::linalg::eigh(a.view_mut()).into();
         assert!((fingerprint(&w) - -71.4747209499407).abs() < 1e-8);
         assert!((fingerprint(&a.abs()) - -9.903934930318247).abs() < 1e-8);
     }
@@ -64,11 +62,11 @@ mod test {
         let mut a = rt::asarray((get_vec::<f64>('a'), [1024, 1024].c(), &device));
 
         // immutable
-        let a_inv = inv(a.view());
+        let a_inv = rt::linalg::inv(a.view());
         assert!((fingerprint(&a_inv) - 143.39005577037764).abs() < 1e-8);
 
         // mutable
-        inv(a.view_mut());
+        rt::linalg::inv(a.view_mut());
         assert!((fingerprint(&a) - 143.39005577037764).abs() < 1e-8);
     }
 }
