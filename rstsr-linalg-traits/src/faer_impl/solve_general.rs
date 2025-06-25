a12bff514e330227cf3425fb74f1e4f5bc2192e1
Zhenyu Zhu ajz34 <ajz34@outlook.com>
2025-06-25 14:14:04 +0800

    linalg: Solve-related functions supports vector (Ix1) RHS (#43)

--- rstsr-linalg-traits/src/faer_impl/solve_general.rs
+++ rstsr-linalg-traits/src/faer_impl/solve_general.rs
@@ -38,48 +38,62 @@ where
 }
 
 #[duplicate_item(
-    ImplType                                                       TrA                                TrB                              ;
-   [T, D, Ra: DataAPI<Data = Vec<T>>, Rb: DataAPI<Data = Vec<T>>] [&TensorAny<Ra, T, DeviceFaer, D>] [&TensorAny<Rb, T, DeviceFaer, D>];
-   [T, D, R: DataAPI<Data = Vec<T>>                             ] [&TensorAny<R, T, DeviceFaer, D> ] [TensorView<'_, T, DeviceFaer, D>];
-   [T, D, R: DataAPI<Data = Vec<T>>                             ] [TensorView<'_, T, DeviceFaer, D>] [&TensorAny<R, T, DeviceFaer, D> ];
-   [T, D,                                                       ] [TensorView<'_, T, DeviceFaer, D>] [TensorView<'_, T, DeviceFaer, D>];
+    ImplType                                                            TrA                                 TrB                              ;
+   [T, DA, DB, Ra: DataAPI<Data = Vec<T>>, Rb: DataAPI<Data = Vec<T>>] [&TensorAny<Ra, T, DeviceFaer, DA>] [&TensorAny<Rb, T, DeviceFaer, DB>];
+   [T, DA, DB, R: DataAPI<Data = Vec<T>>                             ] [&TensorAny<R, T, DeviceFaer, DA> ] [TensorView<'_, T, DeviceFaer, DB>];
+   [T, DA, DB, R: DataAPI<Data = Vec<T>>                             ] [TensorView<'_, T, DeviceFaer, DA>] [&TensorAny<R, T, DeviceFaer, DB> ];
+   [T, DA, DB,                                                       ] [TensorView<'_, T, DeviceFaer, DA>] [TensorView<'_, T, DeviceFaer, DB>];
 )]
 impl<ImplType> SolveGeneralAPI<DeviceFaer> for (TrA, TrB)
 where
     T: ComplexField,
-    D: DimAPI,
+    DA: DimAPI,
+    DB: DimAPI,
 {
-    type Out = Tensor<T, DeviceFaer, D>;
+    type Out = Tensor<T, DeviceFaer, DB>;
     fn solve_general_f(self) -> Result<Self::Out> {
         let (a, b) = self;
         rstsr_assert_eq!(a.ndim(), 2, InvalidLayout, "Currently we can only handle 2-D matrix.")?;
-        rstsr_assert_eq!(b.ndim(), 2, InvalidLayout, "Currently we can only handle 2-D matrix.")?;
+        rstsr_pattern!(b.ndim(), 1..=2, InvalidLayout, "Currently we can only handle 1/2-D matrix.")?;
+        let is_b_vec = b.ndim() == 1;
         let a_view = a.view().into_dim::<Ix2>();
-        let b_view = b.view().into_dim::<Ix2>();
+        let b_view = match is_b_vec {
+            true => b.i((.., None)).into_dim::<Ix2>(),
+            false => b.view().into_dim::<Ix2>(),
+        };
         let result = faer_impl_solve_general_f(a_view.into(), b_view.into())?;
-        Ok(result.into_owned().into_dim::<IxD>().into_dim::<D>())
+        let result = result.into_owned().into_dim::<IxD>();
+        match is_b_vec {
+            true => Ok(result.into_shape(-1).into_dim::<DB>()),
+            false => Ok(result.into_dim::<DB>()),
+        }
     }
 }
 
 #[duplicate_item(
-    ImplType                              TrA                                TrB                             ;
-   ['b, T, D, R: DataAPI<Data = Vec<T>>] [&TensorAny<R, T, DeviceFaer, D> ] [TensorMut<'b, T, DeviceFaer, D>];
-   ['b, T, D,                          ] [TensorView<'_, T, DeviceFaer, D>] [TensorMut<'b, T, DeviceFaer, D>];
-   [    T, D, R: DataAPI<Data = Vec<T>>] [&TensorAny<R, T, DeviceFaer, D> ] [Tensor<T, DeviceFaer, D>       ];
-   [    T, D,                          ] [TensorView<'_, T, DeviceFaer, D>] [Tensor<T, DeviceFaer, D>       ];
+    ImplType                                   TrA                                 TrB                              ;
+   ['b, T, DA, DB, R: DataAPI<Data = Vec<T>>] [&TensorAny<R, T, DeviceFaer, DA> ] [TensorMut<'b, T, DeviceFaer, DB>];
+   ['b, T, DA, DB,                          ] [TensorView<'_, T, DeviceFaer, DA>] [TensorMut<'b, T, DeviceFaer, DB>];
+   [    T, DA, DB, R: DataAPI<Data = Vec<T>>] [&TensorAny<R, T, DeviceFaer, DA> ] [Tensor<T, DeviceFaer, DB>       ];
+   [    T, DA, DB,                          ] [TensorView<'_, T, DeviceFaer, DA>] [Tensor<T, DeviceFaer, DB>       ];
 )]
 impl<ImplType> SolveGeneralAPI<DeviceFaer> for (TrA, TrB)
 where
     T: ComplexField,
-    D: DimAPI,
+    DA: DimAPI,
+    DB: DimAPI,
 {
     type Out = TrB;
     fn solve_general_f(self) -> Result<Self::Out> {
         let (a, mut b) = self;
         rstsr_assert_eq!(a.ndim(), 2, InvalidLayout, "Currently we can only handle 2-D matrix.")?;
-        rstsr_assert_eq!(b.ndim(), 2, InvalidLayout, "Currently we can only handle 2-D matrix.")?;
+        rstsr_pattern!(b.ndim(), 1..=2, InvalidLayout, "Currently we can only handle 1/2-D matrix.")?;
+        let is_b_vec = b.ndim() == 1;
         let a_view = a.view().into_dim::<Ix2>();
-        let b_view = b.view_mut().into_dim::<Ix2>();
+        let b_view = match is_b_vec {
+            true => b.i_mut((.., None)).into_dim::<Ix2>(),
+            false => b.view_mut().into_dim::<Ix2>(),
+        };
         let result = faer_impl_solve_general_f(a_view.into(), b_view.into())?;
         result.clone_to_mut();
         Ok(b)
@@ -87,48 +101,62 @@ where
 }
 
 #[duplicate_item(
-    ImplType                          TrA                               TrB                              ;
-   [T, D, R: DataAPI<Data = Vec<T>>] [TensorMut<'_, T, DeviceFaer, D>] [&TensorAny<R, T, DeviceFaer, D> ];
-   [T, D,                          ] [TensorMut<'_, T, DeviceFaer, D>] [TensorView<'_, T, DeviceFaer, D>];
-   [T, D, R: DataAPI<Data = Vec<T>>] [Tensor<T, DeviceFaer, D>       ] [&TensorAny<R, T, DeviceFaer, D> ];
-   [T, D,                          ] [Tensor<T, DeviceFaer, D>       ] [TensorView<'_, T, DeviceFaer, D>];
+    ImplType                               TrA                                TrB                               ;
+   [T, DA, DB, R: DataAPI<Data = Vec<T>>] [TensorMut<'_, T, DeviceFaer, DA>] [&TensorAny<R, T, DeviceFaer, DB> ];
+   [T, DA, DB,                          ] [TensorMut<'_, T, DeviceFaer, DA>] [TensorView<'_, T, DeviceFaer, DB>];
+   [T, DA, DB, R: DataAPI<Data = Vec<T>>] [Tensor<T, DeviceFaer, DA>       ] [&TensorAny<R, T, DeviceFaer, DB> ];
+   [T, DA, DB,                          ] [Tensor<T, DeviceFaer, DA>       ] [TensorView<'_, T, DeviceFaer, DB>];
 )]
 impl<ImplType> SolveGeneralAPI<DeviceFaer> for (TrA, TrB)
 where
     T: ComplexField,
-    D: DimAPI,
+    DA: DimAPI,
+    DB: DimAPI,
 {
-    type Out = Tensor<T, DeviceFaer, D>;
+    type Out = Tensor<T, DeviceFaer, DB>;
     fn solve_general_f(self) -> Result<Self::Out> {
         let (mut a, b) = self;
         rstsr_assert_eq!(a.ndim(), 2, InvalidLayout, "Currently we can only handle 2-D matrix.")?;
-        rstsr_assert_eq!(b.ndim(), 2, InvalidLayout, "Currently we can only handle 2-D matrix.")?;
+        rstsr_pattern!(b.ndim(), 1..=2, InvalidLayout, "Currently we can only handle 1/2-D matrix.")?;
+        let is_b_vec = b.ndim() == 1;
         let a_view = a.view_mut().into_dim::<Ix2>();
-        let b_view = b.view().into_dim::<Ix2>();
+        let b_view = match is_b_vec {
+            true => b.i((.., None)).into_dim::<Ix2>(),
+            false => b.view().into_dim::<Ix2>(),
+        };
         let result = faer_impl_solve_general_f(a_view.into(), b_view.into())?;
-        Ok(result.into_owned().into_dim::<IxD>().into_dim::<D>())
+        let result = result.into_owned().into_dim::<IxD>();
+        match is_b_vec {
+            true => Ok(result.into_shape(-1).into_dim::<DB>()),
+            false => Ok(result.into_dim::<DB>()),
+        }
     }
 }
 
 #[duplicate_item(
-    ImplType                              TrA                               TrB                             ;
-   ['b, T, D,                          ] [TensorMut<'_, T, DeviceFaer, D>] [TensorMut<'b, T, DeviceFaer, D>];
-   [    T, D,                          ] [TensorMut<'_, T, DeviceFaer, D>] [Tensor<T, DeviceFaer, D>       ];
-   ['b, T, D,                          ] [Tensor<T, DeviceFaer, D>       ] [TensorMut<'b, T, DeviceFaer, D>];
-   [    T, D,                          ] [Tensor<T, DeviceFaer, D>       ] [Tensor<T, DeviceFaer, D>       ];
+    ImplType        TrA                               TrB                              ;
+   ['b, T, DA, DB] [TensorMut<'_, T, DeviceFaer, DA>] [TensorMut<'b, T, DeviceFaer, DB>];
+   [    T, DA, DB] [TensorMut<'_, T, DeviceFaer, DA>] [Tensor<T, DeviceFaer, DB>       ];
+   ['b, T, DA, DB] [Tensor<T, DeviceFaer, DA>       ] [TensorMut<'b, T, DeviceFaer, DB>];
+   [    T, DA, DB] [Tensor<T, DeviceFaer, DA>       ] [Tensor<T, DeviceFaer, DB>       ];
 )]
 impl<ImplType> SolveGeneralAPI<DeviceFaer> for (TrA, TrB)
 where
     T: ComplexField,
-    D: DimAPI,
+    DA: DimAPI,
+    DB: DimAPI,
 {
     type Out = TrB;
     fn solve_general_f(self) -> Result<Self::Out> {
         let (mut a, mut b) = self;
         rstsr_assert_eq!(a.ndim(), 2, InvalidLayout, "Currently we can only handle 2-D matrix.")?;
-        rstsr_assert_eq!(b.ndim(), 2, InvalidLayout, "Currently we can only handle 2-D matrix.")?;
+        rstsr_pattern!(b.ndim(), 1..=2, InvalidLayout, "Currently we can only handle 1/2-D matrix.")?;
+        let is_b_vec = b.ndim() == 1;
         let a_view = a.view_mut().into_dim::<Ix2>();
-        let b_view = b.view_mut().into_dim::<Ix2>();
+        let b_view = match is_b_vec {
+            true => b.i_mut((.., None)).into_dim::<Ix2>(),
+            false => b.view_mut().into_dim::<Ix2>(),
+        };
         let result = faer_impl_solve_general_f(a_view.into(), b_view.into())?;
         result.clone_to_mut();
         Ok(b)
--- rstsr-openblas/src/linalg_traits_impl/solve_triangular.rs
+++ rstsr-openblas/src/linalg_traits_impl/solve_triangular.rs
@@ -6,50 +6,64 @@ use rstsr_linalg_traits::prelude_dev::*;
 /* #region full-args */
 
 #[duplicate_item(
-    ImplType                                                       TrA                                TrB                              ;
-   [T, D, Ra: DataAPI<Data = Vec<T>>, Rb: DataAPI<Data = Vec<T>>] [&TensorAny<Ra, T, DeviceBLAS, D>] [&TensorAny<Rb, T, DeviceBLAS, D>];
-   [T, D, R: DataAPI<Data = Vec<T>>                             ] [&TensorAny<R, T, DeviceBLAS, D> ] [TensorView<'_, T, DeviceBLAS, D>];
-   [T, D, R: DataAPI<Data = Vec<T>>                             ] [TensorView<'_, T, DeviceBLAS, D>] [&TensorAny<R, T, DeviceBLAS, D> ];
-   [T, D,                                                       ] [TensorView<'_, T, DeviceBLAS, D>] [TensorView<'_, T, DeviceBLAS, D>];
+    ImplType                                                            TrA                                 TrB                               ;
+   [T, DA, DB, Ra: DataAPI<Data = Vec<T>>, Rb: DataAPI<Data = Vec<T>>] [&TensorAny<Ra, T, DeviceBLAS, DA>] [&TensorAny<Rb, T, DeviceBLAS, DB>];
+   [T, DA, DB, R: DataAPI<Data = Vec<T>>                             ] [&TensorAny<R, T, DeviceBLAS, DA> ] [TensorView<'_, T, DeviceBLAS, DB>];
+   [T, DA, DB, R: DataAPI<Data = Vec<T>>                             ] [TensorView<'_, T, DeviceBLAS, DA>] [&TensorAny<R, T, DeviceBLAS, DB> ];
+   [T, DA, DB,                                                       ] [TensorView<'_, T, DeviceBLAS, DA>] [TensorView<'_, T, DeviceBLAS, DB>];
 )]
 impl<ImplType> SolveTriangularAPI<DeviceBLAS> for (TrA, TrB, Option<FlagUpLo>)
 where
     T: BlasFloat,
-    D: DimAPI,
+    DA: DimAPI,
+    DB: DimAPI,
     DeviceBLAS: LapackDriverAPI<T>,
 {
-    type Out = Tensor<T, DeviceBLAS, D>;
+    type Out = Tensor<T, DeviceBLAS, DB>;
     fn solve_triangular_f(self) -> Result<Self::Out> {
         let (a, b, uplo) = self;
         rstsr_assert_eq!(a.ndim(), 2, InvalidLayout, "Currently we can only handle 2-D matrix.")?;
-        rstsr_assert_eq!(b.ndim(), 2, InvalidLayout, "Currently we can only handle 2-D matrix.")?;
+        rstsr_pattern!(b.ndim(), 1..=2, InvalidLayout, "Currently we can only handle 1/2-D matrix.")?;
+        let is_b_vec = b.ndim() == 1;
         let a_view = a.view().into_dim::<Ix2>();
-        let b_view = b.view().into_dim::<Ix2>();
+        let b_view = match is_b_vec {
+            true => b.i((.., None)).into_dim::<Ix2>(),
+            false => b.view().into_dim::<Ix2>(),
+        };
         let result = ref_impl_solve_triangular_f(a_view.into(), b_view.into(), uplo)?;
-        return Ok(result.into_owned().into_dim::<IxD>().into_dim::<D>());
+        let result = result.into_owned().into_dim::<IxD>();
+        match is_b_vec {
+            true => Ok(result.into_shape(-1).into_dim::<DB>()),
+            false => Ok(result.into_dim::<DB>()),
+        }
     }
 }
 
 #[duplicate_item(
-    ImplType                              TrA                                TrB                             ;
-   ['b, T, D, R: DataAPI<Data = Vec<T>>] [&TensorAny<R, T, DeviceBLAS, D> ] [TensorMut<'b, T, DeviceBLAS, D>];
-   ['b, T, D,                          ] [TensorView<'_, T, DeviceBLAS, D>] [TensorMut<'b, T, DeviceBLAS, D>];
-   [    T, D, R: DataAPI<Data = Vec<T>>] [&TensorAny<R, T, DeviceBLAS, D> ] [Tensor<T, DeviceBLAS, D>       ];
-   [    T, D,                          ] [TensorView<'_, T, DeviceBLAS, D>] [Tensor<T, DeviceBLAS, D>       ];
+    ImplType                                   TrA                                 TrB                              ;
+   ['b, T, DA, DB, R: DataAPI<Data = Vec<T>>] [&TensorAny<R, T, DeviceBLAS, DA> ] [TensorMut<'b, T, DeviceBLAS, DB>];
+   ['b, T, DA, DB,                          ] [TensorView<'_, T, DeviceBLAS, DA>] [TensorMut<'b, T, DeviceBLAS, DB>];
+   [    T, DA, DB, R: DataAPI<Data = Vec<T>>] [&TensorAny<R, T, DeviceBLAS, DA> ] [Tensor<T, DeviceBLAS, DB>       ];
+   [    T, DA, DB,                          ] [TensorView<'_, T, DeviceBLAS, DA>] [Tensor<T, DeviceBLAS, DB>       ];
 )]
 impl<ImplType> SolveTriangularAPI<DeviceBLAS> for (TrA, TrB, Option<FlagUpLo>)
 where
     T: BlasFloat,
-    D: DimAPI,
+    DA: DimAPI,
+    DB: DimAPI,
     DeviceBLAS: LapackDriverAPI<T>,
 {
     type Out = TrB;
     fn solve_triangular_f(self) -> Result<Self::Out> {
         let (a, mut b, uplo) = self;
         rstsr_assert_eq!(a.ndim(), 2, InvalidLayout, "Currently we can only handle 2-D matrix.")?;
-        rstsr_assert_eq!(b.ndim(), 2, InvalidLayout, "Currently we can only handle 2-D matrix.")?;
+        rstsr_pattern!(b.ndim(), 1..=2, InvalidLayout, "Currently we can only handle 1/2-D matrix.")?;
+        let is_b_vec = b.ndim() == 1;
         let a_view = a.view().into_dim::<Ix2>();
-        let b_view = b.view_mut().into_dim::<Ix2>();
+        let b_view = match is_b_vec {
+            true => b.i_mut((.., None)).into_dim::<Ix2>(),
+            false => b.view_mut().into_dim::<Ix2>(),
+        };
         let result = ref_impl_solve_triangular_f(a_view.into(), b_view.into(), uplo)?;
         result.clone_to_mut();
         Ok(b)
@@ -57,50 +71,64 @@ where
 }
 
 #[duplicate_item(
-    ImplType                          TrA                               TrB                              ;
-   [T, D, R: DataAPI<Data = Vec<T>>] [TensorMut<'_, T, DeviceBLAS, D>] [&TensorAny<R, T, DeviceBLAS, D> ];
-   [T, D,                          ] [TensorMut<'_, T, DeviceBLAS, D>] [TensorView<'_, T, DeviceBLAS, D>];
-   [T, D, R: DataAPI<Data = Vec<T>>] [Tensor<T, DeviceBLAS, D>       ] [&TensorAny<R, T, DeviceBLAS, D> ];
-   [T, D,                          ] [Tensor<T, DeviceBLAS, D>       ] [TensorView<'_, T, DeviceBLAS, D>];
+    ImplType                               TrA                                TrB                               ;
+   [T, DA, DB, R: DataAPI<Data = Vec<T>>] [TensorMut<'_, T, DeviceBLAS, DA>] [&TensorAny<R, T, DeviceBLAS, DB> ];
+   [T, DA, DB,                          ] [TensorMut<'_, T, DeviceBLAS, DA>] [TensorView<'_, T, DeviceBLAS, DB>];
+   [T, DA, DB, R: DataAPI<Data = Vec<T>>] [Tensor<T, DeviceBLAS, DA>       ] [&TensorAny<R, T, DeviceBLAS, DB> ];
+   [T, DA, DB,                          ] [Tensor<T, DeviceBLAS, DA>       ] [TensorView<'_, T, DeviceBLAS, DB>];
 )]
 impl<ImplType> SolveTriangularAPI<DeviceBLAS> for (TrA, TrB, Option<FlagUpLo>)
 where
     T: BlasFloat,
-    D: DimAPI,
+    DA: DimAPI,
+    DB: DimAPI,
     DeviceBLAS: LapackDriverAPI<T>,
 {
-    type Out = Tensor<T, DeviceBLAS, D>;
+    type Out = Tensor<T, DeviceBLAS, DB>;
     fn solve_triangular_f(self) -> Result<Self::Out> {
         let (mut a, b, uplo) = self;
         rstsr_assert_eq!(a.ndim(), 2, InvalidLayout, "Currently we can only handle 2-D matrix.")?;
-        rstsr_assert_eq!(b.ndim(), 2, InvalidLayout, "Currently we can only handle 2-D matrix.")?;
+        rstsr_pattern!(b.ndim(), 1..=2, InvalidLayout, "Currently we can only handle 1/2-D matrix.")?;
+        let is_b_vec = b.ndim() == 1;
         let a_view = a.view_mut().into_dim::<Ix2>();
-        let b_view = b.view().into_dim::<Ix2>();
+        let b_view = match is_b_vec {
+            true => b.i((.., None)).into_dim::<Ix2>(),
+            false => b.view().into_dim::<Ix2>(),
+        };
         let result = ref_impl_solve_triangular_f(a_view.into(), b_view.into(), uplo)?;
-        Ok(result.into_owned().into_dim::<IxD>().into_dim::<D>())
+        let result = result.into_owned().into_dim::<IxD>();
+        match is_b_vec {
+            true => Ok(result.into_shape(-1).into_dim::<DB>()),
+            false => Ok(result.into_dim::<DB>()),
+        }
     }
 }
 
 #[duplicate_item(
-    ImplType                              TrA                               TrB                             ;
-   ['b, T, D,                          ] [TensorMut<'_, T, DeviceBLAS, D>] [TensorMut<'b, T, DeviceBLAS, D>];
-   [    T, D,                          ] [TensorMut<'_, T, DeviceBLAS, D>] [Tensor<T, DeviceBLAS, D>       ];
-   ['b, T, D,                          ] [Tensor<T, DeviceBLAS, D>       ] [TensorMut<'b, T, DeviceBLAS, D>];
-   [    T, D,                          ] [Tensor<T, DeviceBLAS, D>       ] [Tensor<T, DeviceBLAS, D>       ];
+    ImplType        TrA                                TrB                              ;
+   ['b, T, DA, DB] [TensorMut<'_, T, DeviceBLAS, DA>] [TensorMut<'b, T, DeviceBLAS, DB>];
+   [    T, DA, DB] [TensorMut<'_, T, DeviceBLAS, DA>] [Tensor<T, DeviceBLAS, DB>       ];
+   ['b, T, DA, DB] [Tensor<T, DeviceBLAS, DA>       ] [TensorMut<'b, T, DeviceBLAS, DB>];
+   [    T, DA, DB] [Tensor<T, DeviceBLAS, DA>       ] [Tensor<T, DeviceBLAS, DB>       ];
 )]
 impl<ImplType> SolveTriangularAPI<DeviceBLAS> for (TrA, TrB, Option<FlagUpLo>)
 where
     T: BlasFloat,
-    D: DimAPI,
+    DA: DimAPI,
+    DB: DimAPI,
     DeviceBLAS: LapackDriverAPI<T>,
 {
     type Out = TrB;
     fn solve_triangular_f(self) -> Result<Self::Out> {
         let (mut a, mut b, uplo) = self;
         rstsr_assert_eq!(a.ndim(), 2, InvalidLayout, "Currently we can only handle 2-D matrix.")?;
-        rstsr_assert_eq!(b.ndim(), 2, InvalidLayout, "Currently we can only handle 2-D matrix.")?;
+        rstsr_pattern!(b.ndim(), 1..=2, InvalidLayout, "Currently we can only handle 1/2-D matrix.")?;
+        let is_b_vec = b.ndim() == 1;
         let a_view = a.view_mut().into_dim::<Ix2>();
-        let b_view = b.view_mut().into_dim::<Ix2>();
+        let b_view = match is_b_vec {
+            true => b.i_mut((.., None)).into_dim::<Ix2>(),
+            false => b.view_mut().into_dim::<Ix2>(),
+        };
         let result = ref_impl_solve_triangular_f(a_view.into(), b_view.into(), uplo)?;
         result.clone_to_mut();
         Ok(b)
