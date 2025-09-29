f64b9654539e5fd7c168fa25cc319aed3398d771
ajz34 <ajz34@outlook.com>
2025-09-29 17:57:31 +0800

    dtype -- try to add type promotion trait

--- rstsr-dtype-traits/src/lib.rs
+++ rstsr-dtype-traits/src/lib.rs
@@ -3,9 +3,11 @@
 mod ext_float;
 mod ext_num;
 mod ext_real;
+mod promotion;
 mod val_write;
 
 pub use ext_float::*;
 pub use ext_num::*;
 pub use ext_real::*;
+pub use promotion::*;
 pub use val_write::*;
