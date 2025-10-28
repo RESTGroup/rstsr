9a303c1c44cd8302997704576d275a980bc8594c
ajz34 <ajz34@outlook.com>
2025-10-28 10:23:53 +0800

    doc -- try to add doc of to_broadcast

--- rstsr-core/src/tensor/manuplication/broadcast.rs
+++ rstsr-core/src/tensor/manuplication/broadcast.rs
@@ -63,10 +63,46 @@ where
 
 /// Broadcasts an array to a specified shape.
 ///
-/// # See also
+/// # Example
 ///
-/// [Python Array API standard: `broadcast_to`](https://data-apis.org/array-api/2024.12/API_specification/generated/array_api.broadcast_to.html)
+/// The following example demonstrates how to use `to_broadcast` to broadcast a 1-D tensor
+/// (3-element vector) to a 2-D tensor (2x3 matrix) by repeating the original data along a new axis.
+///
+/// ```rust
+/// use rstsr::prelude::*;
+/// let mut device = DeviceCpu::default();
+/// device.set_default_order(RowMajor);
+///
+/// let a = rt::asarray((vec![1, 2, 3], &device));
+/// let b = a.to_broadcast(vec![2, 3]);
+/// let expected = tensor_from_nested![
+///     [1, 2, 3],
+///     [1, 2, 3],
+/// ];
+/// assert!(rt::allclose(&b, &expected, None));
+/// ```
+///
+/// # See also (detailed broadcasting rules)
+///
+/// - [Python Array API standard: Broadcasting rules](https://data-apis.org/array-api/latest/API_specification/broadcasting.html)
+/// - [NumPy: Broadcasting](https://numpy.org/doc/stable/user/basics.broadcasting.html)
+///
+/// # See also (similar function from other crates/libraries)
+///
+/// - [Python Array API standard: `broadcast_to`](https://data-apis.org/array-api/latest/API_specification/generated/array_api.broadcast_to.html)
+/// - [NumPy: `numpy.broadcast_to`](https://numpy.org/doc/stable/reference/generated/numpy.broadcast_to.html)
+/// - [ndarray: `ndarray::broadcast`](https://docs.rs/ndarray/latest/ndarray/struct.ArrayBase.html#method.broadcast)
 pub fn to_broadcast<R, T, B, D, D2>(tensor: &TensorAny<R, T, B, D>, shape: D2) -> TensorView<'_, T, B, D2>
+where
+    D: DimAPI + DimMaxAPI<D2, Max = D2>,
+    D2: DimAPI,
+    R: DataAPI<Data = B::Raw>,
+    B: DeviceAPI<T>,
+{
+    into_broadcast_f(tensor.view(), shape).unwrap()
+}
+
+pub fn broadcast_to<R, T, B, D, D2>(tensor: &TensorAny<R, T, B, D>, shape: D2) -> TensorView<'_, T, B, D2>
 where
     D: DimAPI + DimMaxAPI<D2, Max = D2>,
     D2: DimAPI,
@@ -146,3 +182,23 @@ where
 }
 
 /* #endregion */
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    #[rustfmt::skip]
+    fn doc_broadcast_to() {
+        use rstsr::prelude::*;
+        let mut device = DeviceCpuSerial::default();
+        device.set_default_order(RowMajor);
+
+        let a = rt::asarray((vec![1, 2, 3], &device));
+        let b = a.to_broadcast(vec![2, 3]);
+        let expected = tensor_from_nested![
+            [1, 2, 3],
+            [1, 2, 3],
+        ];
+        assert!(rt::allclose(&b, &expected, None));
+    }
+}
